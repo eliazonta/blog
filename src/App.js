ed43d62653db0745e9661d6718b0a03e180db60f
happyeliia <elia.zontaa@gmail.com>
2022-07-23 23:40:18 +0200

    working...

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from './pages/CreatePost';
 import Login from './pages/Login';
+import { auth } from "./firebase-config";
+import { signOut } from 'firebase/auth';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
   <Router>
+    <nav>
+      <Link to="/">Home</Link>
+      {!isAuth? (
+        <Link to="/login">Login</Link>
+      ):(
+        <>
+        <Link to="/createpost">Create Post</Link>
+        <button onClick={signUserOut}> Log Out</button>
+        </>
+      )}
+    </nav>
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/createpost' element={<CreatePost />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<Home isAuth={isAuth} />} />
+      <Route path='/createpost' element={<CreatePost isAuth={isAuth} />} />
+      <Route path='/login' element={<Login setIsAuth={setIsAuth}/> }/>
     </Routes>
   </Router>
   );
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider} from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 
 export const db = getFirestore(app);
